8147532b2a438db3fe5d3a1b2b11e1a53eff0270
letriandrew <letriandrew@gmail.com>
2023-08-29 01:04:11 -0700

    page set up

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Navbar from './components/Navbar';
 import React from 'react';
 import { BrowserRouter as Router, Routes,  Route } from 'react-router-dom';
 import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
@@ -10,8 +13,10 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' exact element={<Home/>}>
-          </Route>
+          <Route path='/' exact element={<Home/>}/>
+          <Route path='/about' exact element={<About/>}/>
+          <Route path='/contact' exact element={<Contact/>}/>
+          <Route path='/sign-up' exact element={<SignUp/>}/>
         </Routes>
       </Router>
     </>
